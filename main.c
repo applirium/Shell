83a2fb356ce6a879951700300ee943a1b0ea290c
Lukas Stefancik <xtefancik@gstuba.sk>
2024-04-11 04:25:15 +0200

    1.4

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <pthread.h>
 
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -68,20 +68,23 @@ int builtin_help(char **args) {
     return 1;
 }
 
-char*** input_parsing(char* input)
+char* execute(char* input)
 {
-    return NULL;
+    //TODO execution
+    return "doing bullshit\n";
 }
 
 void client() {
     printf("You are client\n");
+
     int sock;
     struct sockaddr_in connection_client;
     char output[BUFFER_SIZE], input[BUFFER_SIZE];
 
     // Create client socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
+    if (sock < 0)
+    {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -100,70 +103,155 @@ void client() {
 
     // Handle client communication
     while(1) {
-        fgets(input, BUFFER_SIZE, stdin);       // Read input from stdin
-        if (strcmp(input, "quit\n") == 0)                   // Check if input contains "quit"
+        memset(input, 0, sizeof(input));
+        memset(output, 0, sizeof(output));
+
+        fgets(input, BUFFER_SIZE, stdin);        // Read input from stdin
+        write(sock, input, strlen(input));      // Send input to the server
+        if (strcmp(input, "quit\n") != 0)                     // Check if input contains "quit"
+        {
+            read(sock, output, sizeof(output));    // Receive output from the server
+            printf("Client: %s", output);                  // Print the received output
+        }
+        else
         {
             close(sock);
+            return;
+        }
+    }
+}
+
+void *handle_server_input(void *arg) {
+    int server_sock = *((int *)arg);
+    char output[BUFFER_SIZE], input[BUFFER_SIZE];
+
+    while (1) {
+        // Read input from stdin
+        memset(input, 0, sizeof(input));
+        memset(output, 0, sizeof(output));
+
+        fgets(input, BUFFER_SIZE, stdin);
+
+        if (strcmp(input, "quit\n") == 0) {
+            // Close the server socket
+            close(server_sock);                 //TODO quit from server
+
+            // Set the flag to stop the server loop
+            keep_running = 0;
             break;
         }
-
-        write(sock, input, strlen(input));      // Send input to the server
-        read(sock, output, sizeof(output));    // Receive output from the server
-        printf("Output: %s", output);                          // Print the received output
+        else
+        {
+            strcpy(output, execute(input));
+            printf("Server: %s",output);
+        }
     }
+
+    return NULL;
+}
+
+void *handle_client(void *arg) {
+    int client_sock = *((int *)arg);
+    char input[BUFFER_SIZE], output[BUFFER_SIZE];
+    ssize_t bytes_received;
+
+    while ((bytes_received = recv(client_sock, input, BUFFER_SIZE, 0)) > 0)
+    {
+        input[bytes_received] = '\0'; // Null-terminate the received data
+
+        // Check if the client wants to quit
+        if (strcmp(input, "quit\n") == 0) {
+            printf("Client disconnected.\n");
+            break;
+        }
+        else
+        {
+            strcpy(output, execute(input));
+            write(client_sock, output, strlen(output));      // Send input to the server     // Echo the message back to the client
+        }
+
+        memset(input, 0, sizeof(input));
+        memset(output, 0, sizeof(output));
+    }
+
+    // Close the client socket
+    close(client_sock);
+    return NULL;
 }
 
 void server() {
     printf("You are server\n");
 
-    int sock, server_sock;
-    struct sockaddr_in connection_server;
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
     pthread_t client_thread_id, input_thread_id;
+    socklen_t client_addr_len;
     int opt = 1;
 
     // Create server socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Set server address structure
-    connection_server.sin_family = AF_INET;
-    connection_server.sin_port = htons(port);
-    connection_server.sin_addr.s_addr = INADDR_ANY;
-
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
-    {
+    // Set server socket options
+    if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
         perror("Setsockopt failed");
         exit(EXIT_FAILURE);
     }
 
+    // Initialize server address structure
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
     // Bind server socket
-    if (bind(sock, (struct sockaddr *) &connection_server, sizeof(connection_server)) < 0)
-    {
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
     // Listen for incoming connections
-    if (listen(sock, MAX_LISTENERS) < 0)
-    {
+    if (listen(server_sock, MAX_LISTENERS) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port %d...\n", port);
 
+    //Create thread to handle server input
+    if (pthread_create(&input_thread_id, NULL, handle_server_input, &server_sock) != 0) {
+        perror("Server input thread creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-    // Main thread continues to handle incoming connections
-    while (1)
-    {
+    // Main server loop
+    while (keep_running) {
+        // Accept incoming connection
+        client_addr_len = sizeof(client_addr);
+        client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_sock < 0) {
+            perror("Accept failed");
+            continue;
+        }
 
+        printf("Client connected: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+        // Create thread to handle client communication
+        if (pthread_create(&client_thread_id, NULL, handle_client, &client_sock) != 0) {
+            perror("Thread creation failed");
+            close(client_sock);
+            continue;
+        }
+
+        // Detach the thread to allow it to run independently
+        pthread_detach(client_thread_id);
     }
 
     // Close the server socket
-    close(sock);
+    close(server_sock);
 }
 
 int main(int argc, char **argv) {
