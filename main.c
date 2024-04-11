5e638b3d515ea6e5118e7c2b5097a8848c56eb10
Lukas Stefancik <xtefancik@gstuba.sk>
2024-04-11 02:47:30 +0200

    connection + client

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -11,15 +12,15 @@
 #define SERVER 0
 #define CLIENT 1
 #define LOCAL 2
-#define BUFFER_SIZE 65536
+#define BUFFER_SIZE 32768
 
 char pathname[50];
 int mode = LOCAL;
 int port = 12345;
+int keep_running = 1;
 
 int builtin_halt(char **args);
 int builtin_help(char **args);
-int builtin_quit(char **args);
 
 char *builtin_str[] = {
         "halt",
@@ -55,7 +56,6 @@ void help()
 int (*builtin_func[]) (char **) = {
         &builtin_halt,
         &builtin_help,
-        &builtin_quit
 };
 
 
@@ -68,78 +68,102 @@ int builtin_help(char **args) {
     return 1;
 }
 
-int builtin_quit(char **args) {
-    return 1;
-}
-
-char*** parser(char* input)
+char*** input_parsing(char* input)
 {
     return NULL;
 }
 
-void client()
-{
+void client() {
     printf("You are client\n");
-}
+    int sock;
+    struct sockaddr_in connection_client;
+    char output[BUFFER_SIZE], input[BUFFER_SIZE];
 
-void server()
-{
-    printf("You are server\n");
-
-    int std_in, std_out, sock, server_sock, opt;
-    char buffer[BUFFER_SIZE];
-    struct sockaddr_in connection_server;
-
+    // Create client socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    connection_server.sin_family = AF_INET;
-    connection_server.sin_port = htons(port);
-    connection_server.sin_addr.s_addr = INADDR_ANY;
-
-    std_in = dup(0);
-    std_out = dup(1);
-    opt = 1;
-
-    if (sock < 0)
-    {
+    if (sock < 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, (char*) &opt, sizeof(opt)) < 0)
+    // Set server address structure
+    connection_client.sin_family = AF_INET;
+    connection_client.sin_port = htons(port);
+    connection_client.sin_addr.s_addr = INADDR_ANY;
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *) &connection_client, sizeof(connection_client)) < 0)
+    {
+        perror("Socket connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Handle client communication
+    while(1) {
+        fgets(input, BUFFER_SIZE, stdin);       // Read input from stdin
+        if (strcmp(input, "quit\n") == 0)                   // Check if input contains "quit"
+        {
+            close(sock);
+            break;
+        }
+
+        write(sock, input, strlen(input));      // Send input to the server
+        read(sock, output, sizeof(output));    // Receive output from the server
+        printf("Output: %s", output);                          // Print the received output
+    }
+}
+
+void server() {
+    printf("You are server\n");
+
+    int sock, server_sock;
+    struct sockaddr_in connection_server;
+    pthread_t client_thread_id, input_thread_id;
+    int opt = 1;
+
+    // Create server socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set server address structure
+    connection_server.sin_family = AF_INET;
+    connection_server.sin_port = htons(port);
+    connection_server.sin_addr.s_addr = INADDR_ANY;
+
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
     {
         perror("Setsockopt failed");
         exit(EXIT_FAILURE);
     }
 
-    if (bind(sock, (struct sockaddr *) &connection_server, sizeof(connection_server)) < 0) {
+    // Bind server socket
+    if (bind(sock, (struct sockaddr *) &connection_server, sizeof(connection_server)) < 0)
+    {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(sock, MAX_LISTENERS) < 0) {
+    // Listen for incoming connections
+    if (listen(sock, MAX_LISTENERS) < 0)
+    {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port %d...\n", port);
-    server_sock = accept(sock, NULL, NULL);
 
-    if (server_sock < 0) {
-        perror("Accept failed");
-        exit(EXIT_FAILURE);
-    }
-    close(sock);
 
-    while(1)
+    // Main thread continues to handle incoming connections
+    while (1)
     {
-        break;
+
     }
 
-}
-
-void local()
-{
-    printf("You are local\n");
+    // Close the server socket
+    close(sock);
 }
 
 int main(int argc, char **argv) {
@@ -199,8 +223,6 @@ int main(int argc, char **argv) {
         case 1:
             client();
             break;
-        default:
-            local();
     }
     return EXIT_SUCCESS;
 }
